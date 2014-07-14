63625e8e62cfe2ff5c9b8f5a6d8c9d465445b76c
Andrew Barisser <barisser@andrews-mbp.lan>
2014-07-14 15:43:07 -0700

    minor changes

--- app/controllers/generate_controller.rb
+++ app/controllers/generate_controller.rb
@@ -1,7 +1,7 @@
 class GenerateController < ApplicationController
 
   def index
-    render json: {message: 'hey something happened'}
+    render json: {message: 'hey something happened'}, status: 200
   end
 
 end
