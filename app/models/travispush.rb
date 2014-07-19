917953361d7361b8b8d537db6c4c98ce7ecc7f02
Andrew Barisser <barisser@andrews-mbp.lan>
2014-07-18 17:36:10 -0700

    travis

--- app/models/travispush.rb
+++ app/models/travispush.rb
@@ -7,7 +7,7 @@ class Travispush
   def self.push(repo_path, access_token)
     Travis.access_token = access_token
     puts "Hello #{Travis::User.current.name}!"
-
+    Travis::User.current.sync
     rails = Travis::Repository.find(repo_path)
     rails.enable
     puts "Enabled Github Repo on Travis-CI"
