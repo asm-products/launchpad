37a71e540319b96d15bf65868b60d10effcfb3b4
Andrew Barisser <barisser@andrews-mbp.lan>
2014-07-21 11:09:48 -0700

    packagejson test updates

--- nodetemplate/hello.js
+++ nodetemplate/hello.js
@@ -2,19 +2,23 @@ var express=require("express");
 var logfmt = require("logfmt");
 var app=express();
 
-app.use(logfmt.requestLogger());
+/*app.use(logfmt.requestLogger());
 
 app.get('/', function(req,res)  {
 
-res.send('Welcome to slipperyslope!');
+//res.send('Welcome to slipperyslope!');
+express.static(__dirname + '/public');
 
 });
+*/
+
+app.use('/', express.static(__dirname + '/public/landing-page'));
 
 var port = Number(process.env.PORT || 5000);
 
 app.listen(port,function() {
 
-  //console.log("Listening on "+port);
-  
+  console.log("Listening on "+port);
+
 
 });
