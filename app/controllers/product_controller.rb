f9c52cb63864110b6e317277aced2a1c9b605a38
Andrew Barisser <barisser@andrews-mbp.lan>
2014-07-21 12:28:10 -0700

    minor

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -81,7 +81,7 @@ class ProductController < ApplicationController
     Travispush.push(travis_path)
     sleep(10.0)
     puts "Adding Readme"
-    readme_contents=Base64.strict_encode64("Welcome to #{title}!")
+    readme_contents="Welcome to #{title}!\n\nThis is your starter NodeJS product page."
     GitPusher.add_file(title,'readme.md',readme_contents)
 
     render json: {message: 'Files Moved to New Repo'}
