d045049ac2fd20fd82eec98092df4e25198ad8e5
Andrew Barisser <barisser@andrews-mbp.lan>
2014-07-24 13:16:03 -0700

    travis exceptions

--- app/models/travispush.rb
+++ app/models/travispush.rb
@@ -9,7 +9,7 @@ class Travispush
     Travis.access_token = "zPfGpvUGrci1cFk198Bdow"
     #client=Travis::Client.new()
     puts "Hello #{Travis::User.current.name}!"
-    Travis::User.current.sync
+    #Travis::User.current.sync
     rails = Travis::Repository.find(repo_path)
     rails.enable
     puts "Enabled Github Repo on Travis-CI"
