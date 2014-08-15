fc156fd2e254b311fd6a76585affe5438b46534c
Charles Pletcher <cpletcher2@gmail.com>
2014-08-14 17:52:15 -0700

    Initial commit of the Node.js/Hapi version

---
+++ api/index.js
@@ -0,0 +1,30 @@
+var Hapi = require('hapi');
+var config = require('../config.json');
+
+var manifest = {
+  servers: [
+    {
+      port: process.env.PORT || 8080
+    }
+  ],
+
+  plugins: {
+    'launchpad-gh': {
+      token: process.env.GIT_HUB_TOKEN || config.GIT_HUB_TOKEN,
+      uri: 'https://api.github.com'
+    },
+    // 'launchpad-heroku': {
+    //   token: process.env.HEROKU_TOKEN || config.HEROKU_TOKEN
+    // }
+  }
+};
+
+Hapi.Pack.compose(manifest, function composeCallback(err, pack) {
+  if (err) {
+    return console.error(err);
+  }
+
+  pack.start();
+
+  console.log('Launchpad listening at ' + pack._servers[0].info.uri);
+});
